4cd39ebe978ad4514c5d49a6165d9e9302bb678c
Jaspreet Singh <105499883+jsaini01@users.noreply.github.com>
2022-08-18 17:52:49 +0530

    witsSites

---
+++ Wits/wits.js
@@ -0,0 +1,61 @@
+function touch() {
+  window.location.assign("https://thewitslab.com/getintouch");
+}
+
+function artificial() {
+  window.location.assign("https://thewitslab.com/tech");
+}
+
+function ar() {
+  window.location.assign("https://thewitslab.com/arvrtech");
+}
+function block() {
+  window.location.assign("https://thewitslab.com/blockchain");
+}
+function machine() {
+  window.location.assign("https://thewitslab.com/machinelearning");
+}
+
+function custom() {
+  window.location.assign(
+    "https://thewitslab.com/custom-application-development"
+  );
+}
+
+function digital() {
+  window.location.assign(
+    "https://thewitslab.com/digital-transformation-services"
+  );
+}
+
+function mobility() {
+  window.location.assign("https://thewitslab.com/enterprise-mobile-solutions");
+}
+
+function design() {
+  window.location.assign("https://thewitslab.com/experience-design-service");
+}
+
+function it() {
+  window.location.assign("https://thewitslab.com/Itac");
+}
+
+function quality() {
+  window.location.assign("https://thewitslab.com/quality-assurance-services");
+}
+
+function technology() {
+  window.location.assign("https://thewitslab.com/quality-assurance-services");
+}
+
+function agile() {
+  window.location.assign("https://thewitslab.com/Agile");
+}
+
+function careers() {
+  window.location.assign("https://thewitslab.com/careers");
+}
+
+function company() {
+  window.location.assign("https://thewitslab.com/about");
+}
